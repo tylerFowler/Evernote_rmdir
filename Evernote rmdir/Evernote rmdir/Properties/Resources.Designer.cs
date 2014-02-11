0e7fb88e6179687149b557943e54860c35cf4b57
tylerFowler <tylerfowler.1337@gmail.com>
2014-02-10 18:05:41 -0600

    deleted an invalid resource, added an icon

--- Evernote rmdir/Evernote rmdir/Properties/Resources.Designer.cs
+++ Evernote rmdir/Evernote rmdir/Properties/Resources.Designer.cs
@@ -63,16 +63,6 @@ namespace Evernote_rmdir.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
-        internal static System.Drawing.Icon icon {
-            get {
-                object obj = ResourceManager.GetObject("icon", resourceCulture);
-                return ((System.Drawing.Icon)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
-        /// </summary>
         internal static System.Drawing.Icon icon_inverted {
             get {
                 object obj = ResourceManager.GetObject("icon_inverted", resourceCulture);
